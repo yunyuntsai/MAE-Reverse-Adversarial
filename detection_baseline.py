9e8bf48ddefbdd08aab3182c7e72b0a832519387
Albert Wen <albertwen19@gmail.com>
2023-02-27 22:39:00 -0500

    /wip minor changes to detection pipeline

--- detection_baseline.py
+++ detection_baseline.py
@@ -10,33 +10,11 @@ from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 
 import torch.utils.data as data
 
-''' 
-- functions are called in evaluate.py in the other git repo
-    - the detection functions are all helpers, only call the xx_vals
-- single_metric_fpr_tpr in evaluate.py, evaluates all attacks over given criteria (l1/targeted/un)
-- same for combined_metric_fpr_tpr, evaluates based on all three baselines
-- tune_criterion_thresholds() function needs further reading
-- .pt is pytorch file format
-
-
-- TODO:
-    - fix file format / TODO issues below
-    - run evaluate on dataset to test
-            $ python evaluate.py [-h] --real_dir REAL_DIR --adv_dir ADV_DIR --title TITLE
-                     [--dataset DATASET] [--base BASE] [--lowbd LOWBD] 
-                     [--upbd UPBD] [--fpr FPR] [--det_opt OPT]
-
-        - real dir is validation set
-        - adv_dir should be some new folder to store 
-        - no 
-'''
-
 plain_transforms = transforms.Compose([
     transforms.ToTensor(),
     transforms.Resize(256), transforms.CenterCrop(224),
     transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD)
 ])
-
 
 # noisy = Noisy.forward # check that this works
 # def l1_detection(model, 
@@ -46,25 +24,19 @@ plain_transforms = transforms.Compose([
 #     return torch.norm(F.softmax(model(transform(img, dataset=dataset))) - 
 #                       F.softmax(model(transform(noisy(img, n_radius, dataset=dataset)))), 1).item()
 
-
-
 def targeted_detection(model, img, true_label, dataset, lr, t_radius, cap=200, margin=20, use_margin=False):
     model.eval()
     # x_var = torch.autograd.Variable(img.clone().cuda(), requires_grad=True)
     x_var = torch.autograd.Variable(img.clone(), requires_grad=True)
 
-    # true_label = model(transform(x_var.clone(), dataset=dataset)).data.max(1, keepdim=True)[1][0].item()
-    # change this function to pass in the true label
 
     optimizer_s = optim.SGD([x_var], lr=lr)
     # target_l = torch.LongTensor([random_label(true_label, dataset=dataset)]).cuda()
     target_l = torch.LongTensor([random_label(true_label, dataset=dataset)])
     counter = 0
 
-    # while model(transform(x_var.clone(), dataset=dataset)).data.max(1, keepdim=True)[1][0].item() == true_label: 
     while model(x_var.clone()).data.max(1, keepdim=True)[1][0].item() == true_label:
         optimizer_s.zero_grad()
-        # output = model(transform(x_var, dataset=dataset))
         output = model(x_var)
         if use_margin:
             target_l = target_l[0].item()
@@ -96,7 +68,6 @@ def untargeted_detection(model,
     model.eval()
     # x_var = torch.autograd.Variable(img.clone().cuda(), requires_grad=True)
     x_var = torch.autograd.Variable(img.clone(), requires_grad=True)
-    # true_label = model(transform(x_var.clone(), dataset=dataset)).data.max(1, keepdim=True)[1][0].item()
     optimizer_s = optim.SGD([x_var], lr=lr)
     counter = 0
     while model(x_var).data.max(1, keepdim=True)[1][0].item() == true_label:
@@ -131,11 +102,9 @@ def l1_vals(model,
             n_radius): # noise radius
     vals = np.zeros(0)
     if attack == "real":
-        # for i in range(lowind, upind):
         for folder, count in enumerate(real_dir, start=lowind):
             if count > upind:
                 break
-            # image_dir = os.path.join(real_dir, str(i) + '_img.pt')
             image_dir = os.path.join(real_dir, str(folder))
             assert os.path.exists(image_dir)
             view_data = torch.load(image_dir)
@@ -146,7 +115,6 @@ def l1_vals(model,
     else:
         cout = upind - lowind
         for i in range(lowind, upind):
-            # image_dir = os.path.join(os.path.join(adv_dir, attack), str(i) + title + '.pt')
             image_dir = os.path.join(os.path.join(adv_dir, attack), str(i) + title)
 
             assert os.path.exists(image_dir)
@@ -165,21 +133,13 @@ def l1_vals(model,
 def targeted_vals(model, dataset, attack, real_dir, adv_dir, targeted_lr, t_radius):
     vals = np.zeros(0)
     if attack == "real":
-        # for i in range(lowind, upind):
-
-        # imagenet_path = "/Users/albertwen/Downloads/mae_data/ImageNet-Data/"
         image_dir = real_dir
         assert os.path.exists(image_dir)
         val_dataset = torchvision.datasets.ImageFolder(root=image_dir, transform=plain_transforms)
         val_dataset_loader = data.DataLoader(val_dataset, batch_size=1, shuffle=False, drop_last=False, num_workers=2)
 
-        for img, label in (val_dataset_loader):
-            # image_dir = os.path.join(real_dir, str(i) + "_img.pt") # TODO: edit
-            # assert os.path.exists(image_dir)
-        
+        for img, label in (val_dataset_loader):        
             model.eval()
-            # print("targeted vals img", img)
-            # print("targeted vals label", label)
             predicted_label = model(img.clone()).data.max(1, keepdim=True)[1][0]
             if label != predicted_label:
                 continue
@@ -196,11 +156,6 @@ def targeted_vals(model, dataset, attack, real_dir, adv_dir, targeted_lr, t_radi
         atk_dataset_loader = data.DataLoader(atk_dataset, batch_size=1, shuffle=False, drop_last=False, num_workers=2)
 
         for img, label in (atk_dataset_loader):
-        # for i in range(lowind, upind):
-            # image_dir = os.path.join(os.path.join(adv_dir, attack), str(i) + title + '.pt')
-            # assert os.path.exists(image_dir)
-            # adv = torch.load(image_dir)
-            # real_label = torch.load(os.path.join(real_dir, str(i) + '_label.pt'))
             model.eval()
             predicted_label = model(img).data.max(1, keepdim=True)[1][0]
             real_label = label
@@ -222,12 +177,6 @@ def untargeted_vals(model, dataset, attack, real_dir, adv_dir, untargeted_lr, u_
         val_dataset = torchvision.datasets.ImageFolder(root=image_dir, transform=plain_transforms)
         val_dataset_loader = data.DataLoader(val_dataset, batch_size=1, shuffle=False, drop_last=False, num_workers=2)
         for img, label in (val_dataset_loader):
-
-        # for i in range(lowind, upind):
-            # image_dir = os.path.join(real_dir, str(i) + '_img.pt')
-            # assert os.path.exists(image_dir)
-            # view_data = torch.load(image_dir)
-
             model.eval()
             val = untargeted_detection(model, img, label, dataset, untargeted_lr, u_radius)
             vals = np.concatenate((vals, [val]))
@@ -239,13 +188,7 @@ def untargeted_vals(model, dataset, attack, real_dir, adv_dir, untargeted_lr, u_
         atk_dataset_loader = data.DataLoader(atk_dataset, batch_size=1, shuffle=False, drop_last=False, num_workers=2)
         for img, label in (atk_dataset_loader):
 
-        # for i in range(lowind, upind):
-            # image_dir = os.path.join(os.path.join(adv_dir, attack), str(i) + title + '.pt')
-            # assert os.path.exists(image_dir)
-            # adv = torch.load(image_dir)
-            # real_label = torch.load(os.path.join(real_dir, str(i) + "_label.pt")) # TODO: edit
             model.eval()
-            # predicted_label = model(transform(adv.clone(), dataset=dataset)).data.max(1, keepdim=True)[1][0]
             predicted_label = model(img).data.max(1, keepdim=True)[1][0]
             real_label = label
             if real_label == predicted_label:
